5066b64e21041e835ded9919782c2176888649b4
Katerina Tuma <1katyakaterina@gmail.com>
2024-08-19 19:19:49 +0300

    Added handleThemeButtonClick

--- js/index.js
+++ js/index.js
@@ -1,12 +1,6 @@
-import {handleThemeButtonClick} from './ui/handlers.js';
-import {handleBurgerButtonClick} from './ui/handlers.js';
-import { updateWarranty } from './ui/updaters.js';
+import { handleThemeButtonClick } from './ui/handlers.js';
 
 export const $app = document.querySelector('#app');
 const $themeButton = document.querySelector('#theme');
-const $burgerButton = document.querySelector('#burger'); 
 
 $themeButton.addEventListener('click', handleThemeButtonClick);
-$burgerButton.addEventListener('click', handleBurgerButtonClick);
-
-updateWarranty();
