384eef87da6e077f76ca5fbbd5e937b9661c252f
Katerina Tuma <1katyakaterina@gmail.com>
2024-08-19 19:19:01 +0300

    Deleted files

--- js/ui/updaters.js
+++
@@ -1,6 +0,0 @@
-import { generateWarranty } from './genetateWarranty.js';
-
-export const updateWarranty = () => {
-  const $warranty = document.querySelector('#warranty');
-  $warranty.innerHTML=generateWarranty();
-}
