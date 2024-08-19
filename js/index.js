e072581dbe42f3206f0bc48758053bc27ea0d92c
Katerina Tuma <1katyakaterina@gmail.com>
2024-08-19 21:30:25 +0300

    Refactored data.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,1 @@
-import { handleThemeButtonClick } from './ui/handlers.js';
 
-export const $app = document.querySelector('#app');
-const $themeButton = document.querySelector('#theme');
-
-$themeButton.addEventListener('click', handleThemeButtonClick);
