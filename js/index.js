ba054a28aaaabdef5dfb4b415955bc7e747214ff
Katerina Tuma <1katyakaterina@gmail.com>
2024-08-23 16:23:24 +0300

    Added Zen page rendering with Header widget

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 import { data } from './data.js';
+import { Header } from './widgets/Header/Header.js';
 import { Download } from './widgets/Download/Download.js';
 
 const $root = document.querySelector('#root');
 
+$root?.insertAdjacentHTML('beforeend', Header(data.primaryInfo));
 $root?.insertAdjacentHTML('beforeend', Download(data.download));
