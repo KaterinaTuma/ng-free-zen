6379d50573f8178a651b7c4e0d0d7c9110e16a4b
Katerina Tuma <1katyakaterina@gmail.com>
2024-08-23 16:20:07 +0300

    Added Theme component

---
+++ js/features/Theme/Theme.js
@@ -0,0 +1,18 @@
+import { IconMoon } from '../../ui/Icons/IconMoon/index.js';
+
+/**
+ * @function Theme
+ * @param {string} className
+ * @returns {string} HTML
+ */
+
+export const Theme = (className) => `
+  <button class="${className}"
+    type="button"
+    id="theme"
+    data-theme = 'light'
+  >
+    ${IconMoon()}
+  </button>
+`;
+
