e0f5e193cf7cb691db8f6e345f67c7916c9ed8a8
Katerina Tuma <1katyakaterina@gmail.com>
2024-08-23 16:20:34 +0300

    Added Burger component

---
+++ js/features/Burger/Burger.js
@@ -0,0 +1,13 @@
+/**
+ * @function Burger
+ * @returns {string} HTML
+ */
+
+export const Burger = () => `
+  <button class="burger"
+    type="button"
+    id="burger"
+  >
+    <span></span>
+  </button>
+`;
