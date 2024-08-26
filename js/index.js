0e160c8812dd23ebca8e21c4d894a1519a7e025a
Katerina Tuma <1katyakaterina@gmail.com>
2024-08-26 13:12:31 +0300

    Added Warranty component

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 import { data } from './data.js';
 import { Header } from './widgets/Header/Header.js';
 import { Download } from './widgets/Download/Download.js';
+import { Warranty } from './widgets/Warranty/Warranty.js';
 
 const $root = document.querySelector('#root');
 
 $root?.insertAdjacentHTML('beforeend', Header(data.primaryInfo));
 $root?.insertAdjacentHTML('beforeend', Download(data.download));
+$root?.insertAdjacentHTML('beforeend', Warranty(data.warranty));
