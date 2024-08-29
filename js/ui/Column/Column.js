9f0dbb441c00a9e536f043a758b3bac033c880e6
Katerina Tuma <1katyakaterina@gmail.com>
2024-08-29 23:45:53 +0300

    Added Column

---
+++ js/ui/Column/Column.js
@@ -0,0 +1,30 @@
+import { ColumnItem } from '../ColumnItem/index.js';
+
+/**
+* @typedef {import('./types').ColumnData} ColumnData
+*/
+
+/**
+* @function Column
+* @param {ColumnData} data
+* @returns {string} HTML
+*/
+
+export const Column = (data) => {
+  const { name, links } = data;
+
+  return `
+    <div class="column">
+      <h3 class="column__title">
+        ${name}
+      </h3>
+      <ul class="column__list">
+        ${links.map((link) => `
+          <li class="column__item">
+            ${ColumnItem(link)}
+          </li>
+        `).join('')}
+      </ul>
+    </div>
+  `;
+};
