1b109e4e31a3186059b994ab044e0ebfb4fb0bf1
Katerina Tuma <1katyakaterina@gmail.com>
2024-08-29 23:04:27 +0300

    Added Care component

--- js/index.js
+++ js/index.js
@@ -2,9 +2,11 @@ import { data } from './data.js';
 import { Header } from './widgets/Header/Header.js';
 import { Download } from './widgets/Download/Download.js';
 import { Warranty } from './widgets/Warranty/Warranty.js';
+import { Care } from './widgets/Care/Care.js';
 
 const $root = document.querySelector('#root');
 
 $root?.insertAdjacentHTML('beforeend', Header(data.primaryInfo));
 $root?.insertAdjacentHTML('beforeend', Download(data.download));
 $root?.insertAdjacentHTML('beforeend', Warranty(data.warranty));
+$root?.insertAdjacentHTML('beforeend', Care(data.care));
---
+++ js/widgets/Care/Care.js
@@ -0,0 +1,34 @@
+import { Title } from '../../ui/Title/index.js';
+import { Image } from '../../ui/Image/index.js';
+
+/**
+* @typedef {import('./types').CareData} CareData
+*/
+
+/**
+* @function Care
+* @param {CareData} data
+* @returns {string} HTML
+*/
+
+export const Care = (data) => {
+  const { title, texts, image } = data;
+
+  return `
+    <section class="care" id="care">
+      <div class="care__wrapper">
+        <div class="care__text-block">
+          <!-- Title -->
+          ${Title(title, 'care__title')}
+          <!-- Texts -->
+          ${texts.map((text) => `
+            <p class="care__copy">${text}</p>
+          `).join('')}
+        </div>
+        <div class="care__image-block">
+          ${Image(image)}
+        </div>
+      </div>
+    </section>
+  `;
+};
