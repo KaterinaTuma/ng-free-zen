9efdb5dabdc96b5d7cf895f80ee4c5ec8cc1e296
Katerina Tuma <1katyakaterina@gmail.com>
2024-08-30 00:26:23 +0300

    Implemented handleThemeClick

---
+++ js/handlers.js
@@ -0,0 +1,43 @@
+import { IconSun } from './ui/Icons/IconSun/index.js';
+import { IconMoon } from './ui/Icons/IconMoon/index.js';
+
+/**
+* @typedef {import('../js/widgets/Clients/types').BrandData} BrandData
+*/
+
+/**
+ *@function handleThemeClick
+ * @param {MouseEvent} event
+ * @param {BrandData} brandsData
+ */
+
+export const handleThemeClick = (event, brandsData) => {
+  const $root = document.querySelector('#root');
+  const $brands = document.querySelectorAll('[data-id="brand"]');
+
+  /** @type { * | EventTarget } */
+  const $themeButton = event.currentTarget;
+  const theme = $themeButton?.dataset.theme;
+
+  if (theme === 'light') {
+    $themeButton.dataset.theme = 'dark';
+    $themeButton.innerHTML = IconSun();
+    $root?.classList.remove('light');
+    $root?.classList.add('dark');
+    $brands.forEach((brand, index) => {
+      brand.src = '';
+      brand.src = brandsData[index].logo.darkSource;
+    });
+  };
+
+  if (theme === 'dark') {
+    $themeButton.dataset.theme = 'light';
+    $themeButton.innerHTML = IconMoon();
+    $root?.classList.remove('dark');
+    $root?.classList.add('light');
+    $brands.forEach((brand, index) => {
+      brand.src = '';
+      brand.src = brandsData[index].logo.lightSource;
+    });
+  };
+};
--- js/index.js
+++ js/index.js
@@ -4,7 +4,9 @@ import { Download } from './widgets/Download/Download.js';
 import { Warranty } from './widgets/Warranty/Warranty.js';
 import { Care } from './widgets/Care/Care.js';
 import { Cashback } from './widgets/Cashback/Cashback.js';
+import { Clients } from './widgets/Clients/Clients.js';
 import { Footer } from './widgets/Footer/Footer.js';
+import { handleThemeClick } from './handlers.js';
 
 const $root = document.querySelector('#root');
 
@@ -13,4 +15,10 @@ $root?.insertAdjacentHTML('beforeend', Download(data.download));
 $root?.insertAdjacentHTML('beforeend', Warranty(data.warranty));
 $root?.insertAdjacentHTML('beforeend', Care(data.care));
 $root?.insertAdjacentHTML('beforeend', Cashback(data.cashback));
+$root?.insertAdjacentHTML('beforeend', Clients(data.clients));
 $root?.insertAdjacentHTML('beforeend', Footer(data.secondaryInfo));
+
+const $themeButton = document.querySelector('#theme');
+$themeButton?.addEventListener('click', (event)  => {
+  handleThemeClick(event, data.clients.brands);
+});
