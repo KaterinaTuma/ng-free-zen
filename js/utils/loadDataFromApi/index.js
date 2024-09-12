47296b0ef2cecd706ccd5e6e5dc65a3f2ed4767b
Katerina Tumakova <1katyakaterina@gmail.com>
2024-09-12 14:27:02 +0300

    Added loadDataFromApi

---
+++ js/utils/loadDataFromApi/index.js
@@ -0,0 +1,1 @@
+export { loadDataFromApi } from './loadDataFromApi.js';
---
+++ js/utils/loadDataFromApi/loadDataFromApi.js
@@ -0,0 +1,23 @@
+import { toCapitalize } from '../index.js';
+
+/**
+ * @typedef {import('../../types').AppData} AppData
+ */
+
+/**
+ * @function loadDataFromApi
+ * @param {string} lang
+ * @return {Promise<AppData|null>}
+ */
+
+export const loadDataFromApi = async (lang) => {
+  try {
+    const moduleName  = 'data' + toCapitalize(lang);
+    const module  = await import(`../../api/${moduleName}.js`);
+    const data = module[moduleName];
+    return data;
+  } catch (error) {
+    console.error(`Failed to load data`, error);
+    return null;
+  }
+};
