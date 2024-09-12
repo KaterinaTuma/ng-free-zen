573ea7ca9269bf28325569d47318e31cbb46ac3c
Katerina Tumakova <1katyakaterina@gmail.com>
2024-09-12 14:25:23 +0300

    Added App component

---
+++ js/app/App.js
@@ -0,0 +1,29 @@
+import { Header } from '../widgets/index.js';
+import { Download } from '../widgets/index.js';
+import { Warranty } from '../widgets/index.js';
+import { Care } from '../widgets/index.js';
+import { Cashback } from '../widgets/index.js';
+import { Clients } from '../widgets/index.js';
+import { Footer } from '../widgets/index.js';
+import { Modal } from '../widgets/index.js';
+
+/**
+ * @typedef {import('../types').AppData} AppData;
+ */
+
+/**
+ * @function App
+ * @param {AppData} data
+ * @returns {string} HTML
+ */
+
+export const App = (data) => `
+  ${Header(data.primaryInfo)}
+  ${Download(data.download)}
+  ${Warranty(data.warranty)}
+  ${Care(data.care)}
+  ${Cashback(data.cashback)}
+  ${Clients(data.clients)}
+  ${Footer(data.secondaryInfo)}
+  ${Modal(data.modal)}
+`;
