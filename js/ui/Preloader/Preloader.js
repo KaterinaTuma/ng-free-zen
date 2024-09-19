e89d82cb20661cd1bea6cb506029ea493f8aa18a
Katerina Tumakova <1katyakaterina@gmail.com>
2024-09-19 10:47:24 +0300

    Added Preloader component

---
+++ js/ui/Preloader/Preloader.js
@@ -0,0 +1,15 @@
+/**
+ * @function Preloader
+ * @returns {string} HTML
+ */
+
+export const Preloader = () => `
+  <div class="preloader">
+    <div class="preloader__wrapper">
+      <div class="preloader__item"></div>
+      <div class="preloader__item"></div>
+      <div class="preloader__item"></div>
+      <div class="preloader__item"></div>
+    </div>
+  </div>
+`;
--- js/ui/index.js
+++ js/ui/index.js
@@ -3,3 +3,4 @@ export { Column } from './Column/index.js';
 export { Image } from './Image/index.js';
 export { Link } from './Link/index.js';
 export { Title } from './Title/index.js';
+export { Preloader } from './Preloader/index.js';
